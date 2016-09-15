853878a7bf5d906d1f04455b787bb561a1f58a21
GrCOTE7 <GrCOTE7@GMail.com>
2016-09-15 10:16:53 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>C2B: Annonce</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- <link rel="stylesheet" href="css/uikit/src/less/uikit.css"/> -->
+  <link rel="stylesheet" href="css/c2b.css">
+</head>
+<body>
+
+<header>
+  <h1>Chemin2Bonheur.com</h1>
+  <em>Nom de code: <b>C2B</b></em>
+  <hr>
+  <h1>Universal Global Method<br>To Happiness*</h1>
+  <h2>Jouons, Tache de Lumière !</h2>
+</header>
+<section>
+  * En français: <b>Méthode Universelle & Globale de Bonheur pour TOUS</b></p>
+  <p>Apportez ainsi toujours plus de Bonheur autour de VOUS, et recevez-en alors 1 milliard de fois plus !</b>  </p>
+  <hr>
+</section>
+<section>
+  <p>Ne sont donc pas considérées ici, aucune des contraintes habituelles et/ou allusions aux critères courants (Langue,
+    race, ethnie, religion, avis politique, niveau social ou scolaire, santé, etc...)</p>
+
+
+  <p>[ToDoLi**: Page d'inscription (Rapide (1 case, 1 clic) & sans engagement)]<br>
+    (Les 'boosteurs (clubadeliens compris) n'auront pas à se réinscrire, et retrouverons enfin, alors, l'intégralité des
+    services auxquels ils avaient accès, et de leurs acquis, que cela soit relatif au nombre de C7 détenus, ou de la
+    réalisation structurelle du groupe construit "à l'époque", cet acquis étant la base de C2B pour une partie... )</p>
+
+
+  <p>Bi1tô, sur vos écrans...</p>
+
+  <p>D'après-vous, est-ce un jeu...?
+    - Et alors...? Demandez-vous MAINTENANT "Pourquoi ?", et faites cela, encore et encore, à chaque réponse de vos
+    toutes
+    vos questions ! Il est recommandé de s'arrêter de jouer à cela plus de 7 itérations par cession, renouvelable
+    cependant à volonté.
+
+    Et c'est gratuit aussi, allais-je oublier, facile pour TOUS, et de plus en plus rapidement
+    effectué, ce qui vous laisse VOUS, entièrement libre de TOUT, HEUREUX(se) !)
+  <p>
+
+  <p>Soyez Partout, commencez prochaînement par entrer dans le chemin2bonheur.com !</p>
+
+  <p>[ToDoLi]URL Git</p>
+
+</section>
+**: ToDoX...X = À faire (Si X...X précisé, par l'initiateur X...X)
+<hr>
+<footer>
+  &copy;GC7 - St. Amand Les Eaux - 09/2016
+</footer>
+<div id="myAffP"></div>
+<script src=" js/jquery-3.1.0.min.js"></script>
+<script src="js/gc7.js"></script>
+<!--<script src="js/uikit/components/sortable.js"></script>-->
+<script src=" js/c2b.js"></script>
+</body>
+</html>
---
+++ js/c2b.js
@@ -0,0 +1,11 @@
+grCote7.tools.chrono.start();
+
+(function () {
+
+    aff('Ok');
+    console.log('Ok');
+
+
+})();
+
+grCote7.tools.chrono.stop();
